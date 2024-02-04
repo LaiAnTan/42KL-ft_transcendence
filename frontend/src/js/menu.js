f2884d602f70db5302b8774c33afed906d8ed950
lwwilliam <lee1052william@gmail.com>
2024-02-04 16:22:00 +0800

    + menu page

--- frontend/src/js/menu.js
+++ frontend/src/js/menu.js
@@ -1,11 +1,21 @@
 export default () => {
-  let img = document.createElement('img');
-  img.src = "../assets/settings.svg";
-  document.body.appendChild(img);
   return `
     <div class="menu-header">
       <h1>MAIN MENU</h1>
-      <img src="./../assets/settings.svg" alt="Setting">
+    </div>
+    <div class="menu-content">
+      <div class="menu-component vs-player">
+        <div class="menu-component-title">VS PLAYER</div>
+        <div class="menu-component-description">PLAY AGAINST OTHER PLAYERS</div>
+      </div>
+      <div class="menu-component vs-ai">
+        <div class="menu-component-title">VS AI</div>
+        <div class="menu-component-description">PLAY 1V1 AGAINST AN AI</div>
+      </div>
+      <div class="menu-component tournament">
+        <div class="menu-component-title">TOURNAMENT</div>
+        <div class="menu-component-description">BRACKET-STYLED TOURNAMENT</div>
+      </div>
     </div>
   `;
 };
