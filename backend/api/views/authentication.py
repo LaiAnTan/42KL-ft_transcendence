55b502b378895f5414bdc8bfef582ebe0c97045d
M4rrs <almasnorazman2803@outlook.com>
2024-02-22 22:32:57 +0800

    + Create user model for simplejwt
    note: Unsure if working. To debug

--- backend/api/views/authentication.py
+++ backend/api/views/authentication.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
 import requests
 import os
 
 UID = os.getenv("UID")
 SECRET = os.getenv("SECRET")
 REDIR_URI = os.getenv("REDIRECT_URI")
+USER = get_user_model()
 
 @api_view(['GET'])
 def get_auth_config(request):
@@ -27,6 +30,11 @@ def postCode(request):
 			ft_me = requests.get("https://api.intra.42.fr/v2/me", headers={"Authorization": "Bearer "+ft_access_token.json()["access_token"]})
 			ft_me_json = ft_me.json()
 			
+			user, created = USER.objects.get_or_create(id=ft_me_json["id"])
+			jwt_refresh = RefreshToken.for_user(user)
+			jwt_token = str(jwt_refresh.access_token)
+			print("JWT Token: ", jwt_token)
+
 			return Response(ft_me_json, status=200)
 		else:
 			# If the request failed, return an error response
--- backend/init.sh
+++ backend/init.sh
@@ -6,6 +6,8 @@ python manage.py makemigrations base
 
 python manage.py migrate base
 
+python manage.py migrate auth
+
 python manage.py runserver 0.0.0.0:8000
 
 echo "Done"
