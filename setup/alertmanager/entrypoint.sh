b237315f434317d17debba2e3ad5ef8048422911
laian <tanlaian2004@gmail.com>
2024-03-15 14:53:43 +0800

    feat: prometheus alerts
    + configured prometheus alert manager to POST alert to backend when postgres is down

--- setup/alertmanager/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# script generates the alertmanager.yml file and starts the process
-
-envsubst < alertmanager.yml.tpl > alertmanager.yml
-
-/bin/alertmanager --config.file=/etc/alertmanager.yml
