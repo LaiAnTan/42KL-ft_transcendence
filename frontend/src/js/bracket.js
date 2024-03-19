97ffe0e42fc4440a9ed91c052a30378598432d02
lwwilliam <lee1052william@gmail.com>
2024-03-19 20:18:54 +0800

    * tournament backet ui

--- frontend/src/js/bracket.js
+++ frontend/src/js/bracket.js
@@ -1,6 +1,8 @@
 import { loadCSS, router } from "./main.js";
 
 export default () => {
+  let config_palette = localStorage.getItem("palette");
+  loadCSS("src/css/palettes/" + config_palette + ".css");
   loadCSS("src/css/bracket.css");
 
   function pollBackend() {
@@ -8,13 +10,12 @@ export default () => {
         .then(response => response.json())
         .then(data => {
           const rooms = data.tourney;
-          console.log(rooms);
-          const matchups = Object.values(rooms);
-          const boxes = document.querySelectorAll('.box');
-          
-          matchups.forEach((matchup, index) => {
-            const teams = matchup.join(' vs ');
-            boxes[index].innerHTML = `<p>${teams}</p>`;
+          const boxes = document.querySelectorAll('.box-content');
+          Object.keys(rooms).forEach((roomId, index) => {
+            const players = rooms[roomId].slice(0, 2); // Get the first two players in the room
+            players.forEach((player, i) => {
+              boxes[index * 2 + i].textContent = player; // Assign player to corresponding box
+            });
           });
         })
         .catch(error => {
@@ -26,27 +27,58 @@ export default () => {
   // setInterval(pollBackend, 5000);
 
   return `
+  <button data-link="/menu" type="button" class="go-back-button scale-up ml-4" style="z-index: 1">
+    <p class="description scale-up cursor-pointer">GO BACK</p>
+  </button>
+  <div class="menu-header unselectable">
+    <p class="text-center menu-header-title h-100 my-4">TOURNAMENT</p>
+  </div>
+  <button title="To dashboard" data-link="/dashboard?username=${sessionStorage.getItem('username')}" type="submit" class="user-profile unselectable scale-up mr-4" style="z-index: 1">
+    <div class="user-img"><img src="${sessionStorage.getItem('profile_pic')}"></img></div>
+    <p class="description cursor-pointer">${sessionStorage.getItem('display_name')}</p>
+  </button>
   <div class="wrapper">
     <div class="col">
       <div class="row">
-        <div class="box"><p>a</p></div>
-        <div class="box"><p>b</p></div>
-        <div class="box"><p>c</p></div>
-        <div class="box"><p>d</p></div>
+        <div class="box">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
+        <div class="box">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
+        <div class="box">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
+        <div class="box">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
       </div>
       <div class="row">
         <div class="line start"></div>
         <div class="line start"></div>
       </div>
       <div class="row">
-        <div class="box middle"><p>e</p></div>
-        <div class="box middle"><p>f</p></div>
+        <div class="box middle">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
+        <div class="box middle">
+          <div class="box-content">P1</div>
+          <div class="box-content">P2</div>
+        </div>
       </div>
       <div class="row">
         <div class="line end"></div>
       </div>
       <div class="row">
-        <div class="box"><p>g</p></div>
+        <div class="box">
+          <div class="box-content box-content-up">P1</div>
+          <div class="box-content">P2</div>
+        </div>
       </div>
     <div>
   </div>
