6f61d7940c8979f93eb7b140a4a2307cfe17badd
M4rrs <almasnorazman2803@outlook.com>
2024-03-24 23:51:07 +0800

    * Removed login page, sign in now in root.

--- frontend/src/js/home.js
+++ frontend/src/js/home.js
@@ -5,9 +5,32 @@ let keydownHandler = null;
 export default () => {
 	loadCSS("src/css/home.css");
 	
+	let authConfig = {};
+
+	fetch("https://localhost:8000/api/authConfig")
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+		authConfig = data;
+	})
+	.catch((error) => {
+		console.error("Error:", error);
+	});
+
+	function signin42() {
+		if (!authConfig.clientID || !authConfig.redirectURI) {
+			console.log("config missing");
+		}
+
+		const { clientID, redirectURI} = authConfig;
+		const scopes = 'public';
+		const authURL = `https://api.intra.42.fr/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=code&scope=${scopes}`;
+		window.location.href = authURL;
+	}
+
 	if (!keydownHandler) {
 		keydownHandler = (event) => {
-			navigate('/login');
+			signin42();
 			document.removeEventListener('keydown', keydownHandler);
 			keydownHandler = null;
 		};
@@ -20,6 +43,6 @@ export default () => {
 		<div class="title-bg">DING DONG</div>
 		<div class="title">DING DONG</div>
 	</div>
-	<p>Press any key to continue</p>
+	<p>Press any key to sign in</p>
 </div>`;
 };
